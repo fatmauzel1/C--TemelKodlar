4050dca220b8320a735f31b196cb0e71a7d9a7d0
fatmauzel1 <fatmauzel6@gmail.com>
2024-04-26 02:29:45 +0300

    KullanicidanDegerAlma

---
+++ DegerAlma/KullanicidanDegerAlma.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kullanicidanDegerAlma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // Readline 
+
+            Console.WriteLine("lütfen adınızı giriniz");
+
+            string isim = Console.ReadLine();
+
+
+            Console.WriteLine("Soyisminizi giriniz");
+
+            string soyisim = Console.ReadLine();
+
+
+            Console.WriteLine("Kullanıcının ismi" + isim);
+            Console.WriteLine("Kullanıcının soyismi" + soyisim);
+            
+
+
+            Console.ReadLine();
+        }
+    }
+}
