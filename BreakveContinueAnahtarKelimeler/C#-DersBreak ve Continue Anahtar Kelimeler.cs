2545532ceeeb3bc3c8131c35a412cc7eec5433e1
fatmauzel1 <fatmauzel6@gmail.com>
2024-05-06 23:53:36 +0300

    BreakveContinueAnahtarKelimeleri

---
+++ BreakveContinueAnahtarKelimeler/C#-DersBreak ve Continue Anahtar Kelimeler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Break_ve_Continue_Anahtar_Kelimeler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             
+            // Break anahtar kelimesi
+            // Continue anahtar kelimesi 
+
+            for (int a = 1; a<=10; a++)
+            {
+                if (a == 7)
+                {
+                    break;
+                }
+                Console.WriteLine("a'nın değeri :"+a);
+            }
+
+
+            for (int i = 1; i<=10; i++)
+            {
+                if(i == 5)
+                {
+                    continue;
+                }
+                Console.WriteLine("i'nın değeri :" + i);
+            }
+             Console.ReadLine();
+
+        }   
+    }
+}
